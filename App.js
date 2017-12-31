d7f0b93816ce67499462aa01901108b170fbd2db
Josh Justice <jjustice@bignerdranch.com>
2017-12-31 06:04:02 -0500

    Create empty NewMessageForm component
    Instead of adding a TextInput directly, let's create a component to wrap the entire header of the app, with the TextInput and the save Button. To take a small step, first we create it as an empty component.

--- App.js
+++ App.js
@@ -8,11 +8,13 @@ import React, { Component } from 'react';
 import {
   View,
 } from 'react-native';
+import NewMessageForm from './NewMessageForm';
 
 export default class App extends Component {
   render() {
     return (
       <View>
+        <NewMessageForm />
       </View>
     );
   }
