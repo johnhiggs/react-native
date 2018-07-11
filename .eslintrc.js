7152cee1780902f45db9c0ef3feb5679ab98b8d2
Josh Justice <me@codingitwrong.com>
2018-07-11 16:33:38 -0400

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'codingitwrong',
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'detox',
+    'jest',
+  ],
+  'env': {
+    'detox/detox': true,
+    'jest/globals': true,
+  },
+  'rules': {
+    'react/prop-types': 'off',
+  },
+};
